5c7a268f131ae6340e578da32d062837ccc7a8c7
Dhrax <davidflpc@gmail.com>
2019-11-28 00:54:42 +0100

    Se ha construido el layout del formulario

--- app/src/main/java/com/daisa/trabajo1/AnhadirJuego.java
+++ app/src/main/java/com/daisa/trabajo1/AnhadirJuego.java
@@ -3,6 +3,7 @@ package com.daisa.trabajo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AnhadirJuego extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class AnhadirJuego extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anhadir_juego);
+
+        Button btVolver = findViewById(R.id.btVolver);
     }
 
 
